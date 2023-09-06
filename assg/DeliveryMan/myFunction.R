9b68b967a00008cc91ddeb252246c1027b422c66
FKnorring <fille.vk@gmail.com>
2023-09-06 16:49:05 +0200

    slow solution

--- assg/DeliveryMan/myFunction.R
+++ assg/DeliveryMan/myFunction.R
@@ -2,17 +2,17 @@ pq <- function() {
   # Internal lists to hold items and their priorities
   items <- list()
   priorities <- numeric()
-
+  
   # Enqueue an item with a given priority
   enqueue <- function(item, priority) {
     # Find the correct position to insert the item
     idx <- sum(priorities <= priority) + 1
-
+    
     # Insert the item and its priority at the correct position
     items <<- append(items, list(item), after = idx - 1)
     priorities <<- append(priorities, priority, after = idx - 1)
   }
-
+  
   # Dequeue the highest priority item
   dequeue <- function() {
     if (length(items) == 0) {
@@ -23,7 +23,7 @@ pq <- function() {
     priorities <<- priorities[-1]
     return(item)
   }
-
+  
   # Peek at the highest priority item without removing it
   peek <- function() {
     if (length(items) == 0) {
@@ -31,12 +31,12 @@ pq <- function() {
     }
     return(items[[1]])
   }
-
+  
   # Check if the queue is empty
   is_empty <- function() {
     return(length(items) == 0)
   }
-
+  
   # Return the methods as a list
   return(list(
     enqueue = enqueue,
@@ -61,19 +61,10 @@ myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
   } else {
     carInfo$mem$goal <- packageMatrix[carInfo$load, c(3, 4)]
   }
-
-  if (carInfo$x == carInfo$mem$goal[[1]] && carInfo$y == carInfo$mem$goal[[2]]) {
-    carInfo$nextMove <- 5
-    return(carInfo)
-  }
-
+  
   # Get the path using A* algorithm
-  print("Started search")
-  print("Goal:")
-  print(carInfo$mem$goal)
-  print("")
   path <- aStar(trafficMatrix, carInfo, packageMatrix)
-  print("Search ended")
+  
   # If path is not empty, determine the next move
   if (length(path) > 0) {
     nextCoord <- path[[1]]
@@ -81,7 +72,7 @@ myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
     # Determine the direction based on the difference between current and next coordinates
     deltaX <- nextCoord[1] - currentCoord[1]
     deltaY <- nextCoord[2] - currentCoord[2]
-
+    
     if (deltaX == 1) {
       carInfo$nextMove <- 6 # Right
     } else if (deltaX == -1) {
@@ -95,8 +86,6 @@ myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
     # If no path is found or the goal is already reached, set nextMove to a default value
     carInfo$nextMove <- 5
   }
-  print("Move")
-  print(carInfo$nextMove)
   return(carInfo)
 }
 
@@ -105,10 +94,10 @@ myFunction <- function(trafficMatrix, carInfo, packageMatrix) {
 h <- function(carInfo, frontierNode) {
   goalX <- carInfo$mem$goal[1]
   goalY <- carInfo$mem$goal[2]
-
+  
   frontierX <- frontierNode[1]
   frontierY <- frontierNode[2]
-
+  
   return(abs(goalX - frontierX) + abs(goalY - frontierY))
 }
 
@@ -116,21 +105,20 @@ h <- function(carInfo, frontierNode) {
 g <- function(carInfo, frontierNode, trafficMatrix) {
   carX <- carInfo$x
   carY <- carInfo$y
-
+  
   frontierX <- frontierNode[1]
   frontierY <- frontierNode[2]
-
+  
   # Determine the direction of movement
   if (carX == frontierX) { # Vertical movement
-    return(trafficMatrix$vroads[min(carY, frontierY), carX])
+    return(trafficMatrix$vroads[carX, min(carY, frontierY)])
   } else { # Horizontal movement
-    return(trafficMatrix$hroads[carY, min(carX, frontierX)])
+    return(trafficMatrix$hroads[min(carX, frontierX), carY])
   }
 }
 
-
 f <- function(carInfo, frontierNode, trafficMatrix) {
-  return(g(carInfo, frontierNode, trafficMatrix) + h(carInfo, frontierNode) * 3)
+  return(g(carInfo, frontierNode, trafficMatrix) + h(carInfo, frontierNode))
 }
 
 carCords <- function(carInfo) {
@@ -140,67 +128,63 @@ carCords <- function(carInfo) {
 getNeighbors <- function(carNode, dim) {
   x <- carNode[[1]]
   y <- carNode[[2]]
-
-  neighbors <- list()
-
+  
+  neighbors <- vector("list", 4)
+  idx <- 1
+  
   if (x > 1) {
-    neighbors <- append(neighbors, list(c(x - 1, y)))
+    neighbors[[idx]] <- c(x - 1, y)
+    idx <- idx + 1
   }
-
+  
   if (x <= dim) {
-    neighbors <- append(neighbors, list(c(x + 1, y)))
+    neighbors[[idx]] <- c(x + 1, y)
+    idx <- idx + 1
   }
-
+  
   if (y > 1) {
-    neighbors <- append(neighbors, list(c(x, y - 1)))
+    neighbors[[idx]] <- c(x, y - 1)
+    idx <- idx + 1
   }
-
+  
   if (y <= dim) {
-    neighbors <- append(neighbors, list(c(x, y + 1)))
+    neighbors[[idx]] <- c(x, y + 1)
   }
-
+  
   return(neighbors)
 }
 
-
-
-
 aStar <- function(trafficMatrix, carInfo, packageMatrix) {
   q <- pq()
-
+  
   # add start node to queue
   # node looks like [carCoords, cost, path]
   startNode <- list(cords = carCords(carInfo), cost = 0, path = list())
-  visited <- list()
-
+  gridDim <- nrow(trafficMatrix$hroads)
+  visitedMatrix <- matrix(FALSE, nrow = gridDim+1, ncol = gridDim+1)
+  visitedMatrix[carInfo$x, carInfo$y] <- TRUE
+  
   q$enqueue(startNode, 0)
-
+  
   while (!q$is_empty()) {
-    # get node with lowest cost
     frontierNode <- q$dequeue()
     cords <- frontierNode$cords
-    cost <- frontierNode$cost
-    path <- frontierNode$path
-
-    # check if goal
+    
     if (all(cords == carInfo$mem$goal)) {
-      return(path)
+      return(frontierNode$path)
     }
-
-    visited <- append(visited, list(cords))
-
-    # get neighbors
-    gridDim <- nrow(trafficMatrix$hroads)
+    
     neighbors <- getNeighbors(cords, gridDim)
-
-    # add neighbors to queue
-    for (i in 1:length(neighbors)) {
-      if (any(sapply(visited, function(x) all(x == neighbors[[i]])))) {
-        next
-      }
-      neighbor <- neighbors[[i]]
-      newCost <- cost + f(carInfo, neighbor, trafficMatrix)
-      q$enqueue(list(cords = neighbor, cost = newCost, path = c(path, list(neighbor))), newCost)
+    
+    for (neighbor in neighbors) {
+      if (is.null(neighbor)) next
+      if (visitedMatrix[neighbor[[1]], neighbor[[2]]]) next
+      
+      visitedMatrix[neighbor[[1]], neighbor[[2]]] <- TRUE
+      newCost <- frontierNode$cost + f(carInfo, neighbor, trafficMatrix)
+      q$enqueue(list(cords = neighbor, cost = newCost, path = c(frontierNode$path, list(neighbor))), newCost)
     }
   }
+  print("Search failed")
+  return(NULL)
 }
